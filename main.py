2928f5baa04666afb17503668976d7fd75e38c63
Sibdroid <ryabovglebrg@gmail.com>
2024-04-05 11:17:18 +0200

    fix: fix no nan issue

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def xlsx_to_data(path: str) -> pd.DataFrame:
     candidates = []
     for column in df.columns:
         column_data = df[column].tolist()
+        if np.nan not in column_data:
+            if "TOTALS" in column_data:
+                column_data.insert(column_data.index("TOTALS")+1, np.nan)
+            else:
+                second_string = [i for i in column_data
+                                 if isinstance(i, str)][1]
+                column_data.insert(column_data.index(second_string), np.nan)
         nan_index = column_data.index(np.nan)
         first_candidate = remove_nans(column_data[0:nan_index])
         second_candidate = remove_nans(column_data[nan_index+1:])
@@ -31,12 +38,12 @@ def xlsx_to_data(path: str) -> pd.DataFrame:
 print(xlsx_to_data("2024-pp-republican-sullivan_1.xlsx").to_string())
 
 # belknap - made for this
-# carroll - fails because no nan
+# carroll - works kind of
 # cheshire - works kind of
-# coos - fails because no nan
-# grafton - fails because no nan
-# hillsborough - fails because no nan
-# merrimack - fails because no nan
-# rockingham - fails because no nan
-# strafford - fails because no nan
+# coos - works kind of
+# grafton - works kind of
+# hillsborough - fails for weird reason, idk
+# merrimack - fails for weird reason, idk
+# rockingham - fails for weird reason, idk
+# strafford - fails for weird reason, idk
 # sullivan - works kind of
